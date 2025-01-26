bbe033dd21b1e0b1b3a5b498c01fedab95d4ee55
omirzhan001 <omirzhan2@gmail.com>
2025-01-27 00:35:55 +0500

    1

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,18 +1,24 @@
 import { useState, useEffect } from "react";
 
 function Todo() {
-const [tasks, setTasks] = useState([
+
+const [tasks, setTasks] = useState(() => {
+  const savedTasks = localStorage.getItem("tasks");
+  return savedTasks ? JSON.parse(savedTasks) : [
     { id: 1, name: "Write Essay", done: false },
     { id: 2, name: "One Hour CSS Course Online", done: false },
     { id: 3, name: "Buy One Way Tickets to San Francisco", done: false },
     { id: 4, name: "Go to Gym", done: false },
     { id: 5, name: "Buy Groceries", done: false },
-  ]);
+  ]
+});
+
+  // Сохраняем задачи в localStorage при их изменении
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  
+
   return (
     <>
       <header>
