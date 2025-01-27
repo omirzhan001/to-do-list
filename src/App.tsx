2474e60ecf2f730c974c70e6110f0c7ab75d1e25
omirzhan001 <omirzhan2@gmail.com>
2025-01-28 00:27:55 +0500

    027

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,55 @@
 import './App.css'
 import Header from './components/Header'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import Todo from './pages/Todo'
 import Done from './pages/Done'
 import Trash from './pages/Trash'
+
 function App() {
-
-
   return (
     <div className='container'>
       <BrowserRouter>
-      
-      <Header/>
-      <nav >
-        <ul className='nav '>
-          <li className='button__nav  '>
-            <Link to="/" className='link_app'>To Do</Link>
-          </li>
-          <li className='button__nav'>
-            <Link to="/Done" className='link_app'>Done</Link>
-          </li>
-          <li className='button__nav '>
-            <Link to="/Trash" className='link_app'>Trash</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes >
-        <Route path="/" element={<Todo />} />
-        <Route path="/Done" element={<Done />} />
-        <Route path="/Trash" element={<Trash />} />
-      </Routes>
-    </BrowserRouter >
+        <Header />
+        <nav>
+          <ul className='nav'>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? 'button__nav active' : 'button__nav'
+                }
+              >
+                To Do
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Done"
+                className={({ isActive }) =>
+                  isActive ? 'button__nav active' : 'button__nav'
+                }
+              >
+                Done
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Trash"
+                className={({ isActive }) =>
+                  isActive ? 'button__nav active' : 'button__nav'
+                }
+              >
+                Trash
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Todo />} />
+          <Route path="/Done" element={<Done />} />
+          <Route path="/Trash" element={<Trash />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
