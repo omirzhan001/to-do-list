c0b377851aaea22e09d055d15cfb0e76a2eb337d
Omirzhan <omirzhan2@gmail.com>
2025-01-28 11:49:14 +0500

    1147

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import TrashIcon from "../icon";
-import Header from "../../components/Header/";
 
 function Todo() {
   const [tasks, setTasks] = useState(() => {
